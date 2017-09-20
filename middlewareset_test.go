0df4d07770b5fdcba32000afc0bc5aecbb3bdc0c
nasa9084 <nasa9084@live.jp>
2017-09-20 13:47:29 +0900

    call Set content type
    for coverage

--- middlewareset_test.go
+++ middlewareset_test.go
@@ -12,11 +12,13 @@ import (
 const hello = "hello, world"
 
 var testHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(200)
 	w.Write([]byte(hello))
 })
 
 func testHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(200)
 	w.Write([]byte(hello))
 }
